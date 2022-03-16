bd9dfd117bbe0603a540090153df1571d75a2d9f
Thien Quoc <talavipcute5555@gmail.com>
2022-03-16 12:06:33 +0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using PagedList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Tuan04_TonQuocBinh.Models;
+
+namespace Tuan04_TonQuocBinh.Controllers
+{
+    public class HomeController : Controller
+    {
+        MyDataDataContext data = new MyDataDataContext();
+        public ActionResult Index(int? page)
+        {
+            if (page == null) page = 1;
+            var all_sach = (from s in data.Saches select s).OrderBy(m => m.masach); 
+            int pageSize = 2;
+            int pageNum = page ?? 1;
+            return View(all_sach.ToPagedList(pageNum, pageSize));
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Tuan04_TonQuocBinh.MvcApplication" Language="C#" %>
